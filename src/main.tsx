18ebf26f6eff487ee920701ab2bb57bcce56de22
Dmytro Lymar <dmytrolymar901@gmail.com>
2025-12-17 12:19:34 +0200

    switch to HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./styles/global.scss";
 import App from "./App.tsx";
 import { AuthProvider } from "./auth/AuthProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
